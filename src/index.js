cb368741a4d4e1888a6ac18f5169fc6ede0dd77e
vedikabajaj05 <146482352+vedikabajaj05@users.noreply.github.com>
2025-05-23 15:44:45 +0545

    index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'C:/Users/vedik/OneDrive/Desktop/centrumq/centrumq/src/styles/styles.scss';
-import App from 'C:/Users/vedik/OneDrive/Desktop/centrumq/centrumq/src/App.js';
+import './styles/styles.scss';
+import App from './App';
 
 console.log('Index.js loaded');
 const root = ReactDOM.createRoot(document.getElementById('root'));
